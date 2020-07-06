82a12f9fa9826f440bebcd40beaf91418b4270ce
Miikka Niemelä <miikka.niemela@orangit.fi>
2020-07-06 14:13:38 +0300

    Fix redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import * as reducers from './reducers'
 
-const createStoreWithMiddlewares = applyMiddleware(thunk)(createStore)
-
-const store = createStoreWithMiddlewares(combineReducers(reducers))
+const store = createStore(
+  combineReducers(reducers),
+  composeWithDevTools(applyMiddleware(thunk))
+)
 
 export default store
