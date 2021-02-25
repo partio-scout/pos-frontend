b3be178cf85c32e3867e9cbf4028a77a8cd0f704
Kaisa Korpela <kaisa.korpela@orangit.fi>
2021-02-25 10:06:00 +0200

    Remove unused import

--- src/views/AcceptTasks/group.js
+++ src/views/AcceptTasks/group.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import styled, { keyframes } from 'styled-components'
 import { useParams } from 'react-router-dom'
 import {
