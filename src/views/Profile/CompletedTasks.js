f63d2a33b8276e223014213beb3e15a0177e72fe
Linnea Myllynen <linnea.myllynen@orangit.fi>
2021-09-01 10:54:04 +0300

    Add translations to profilepage

--- src/views/Profile/CompletedTasks.js
+++ src/views/Profile/CompletedTasks.js
@@ -47,12 +47,16 @@ const AccordionList = ({
   const userTasks = useSelector(state => state.tasks)
   const generalTranslations = useSelector(state => state.translations.yleiset)
   const taskGroup = itemsByGuid[taskGroupGuid]
+  const getTranslation = taskOrTaskGroup => {
+    return taskOrTaskGroup.languages.find(x => x.lang === language)
+  }
 
   const status = getTaskGroupStatus(
     taskGroup.item,
     userTasks,
     getTermInLanguage(generalTranslations, 'done', language)
   )
+  const taskTranslation = getTranslation(taskGroup.item)
 
   return (
     <Accordion key={taskGroupGuid} allowZeroExpanded>
@@ -61,7 +65,7 @@ const AccordionList = ({
           <AccordionItemButton>
             {!taskGroup.item.taskgroups.length > 0 ? (
               <ListItem
-                title={taskGroup.item.title}
+                title={taskTranslation ? taskTranslation.title : taskGroup.item.title}
                 itemType={ITEM_TYPES.TASK_GROUP}
                 ageGroupGuid={taskGroup.ageGroupGuid}
                 language={language}
@@ -71,7 +75,7 @@ const AccordionList = ({
               />
             ) : (
               <ListItem
-                title={taskGroup.item.title}
+                title={taskTranslation ? taskTranslation.title : taskGroup.item.title}
                 itemType={ITEM_TYPES.TASK_GROUP}
                 ageGroupGuid={taskGroup.ageGroupGuid}
                 language={language}
@@ -112,11 +116,17 @@ const AccordionList = ({
 
 const TaskList = ({ tasks, taskGroup, language }) => {
   return tasks.map(task => {
+    
+    const getTranslation = taskOrTaskGroup => {
+      return taskOrTaskGroup.languages.find(x => x.lang === language)
+    }
+    const taskTranslation = getTranslation(task.item)
+    
     return (
       <ListItem
         key={task.guid}
         guid={task.guid}
-        title={task.item.title}
+        title={taskTranslation ? taskTranslation.title : task.item.title}
         itemType={ITEM_TYPES.TASK}
         ageGroupGuid={taskGroup.ageGroupGuid}
         language={language}
