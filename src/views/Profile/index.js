5b1b4788a9d798708c8e52dcb76f646b27a56783
Linnea Myllynen <linnea.myllynen@orangit.fi>
2022-12-22 14:30:50 +0200

    small fix

--- src/views/Profile/index.js
+++ src/views/Profile/index.js
@@ -396,6 +396,7 @@ const Profile = () => {
             taskgroupsMarkedCompleted={
               taskgroupsMarkedCompletedWhenAgeGroupMarkedCompleted
             }
+            actionsComponent={actionTypes.openTaskActions}
           />
         </BodyContent>
       </Content>
